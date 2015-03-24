50d620e1db9533a614402eb2def9b519e43260ee
Senthil <senthil@koding.com>
2015-03-24 15:55:49 -0700

    create 'eventexportertest' to hold FakeClient

--- fakeexporter_test.go
+++ fakeexporter_test.go
@@ -1,30 +1,19 @@
-package eventexporter_test
+package eventexporter
 
 import (
 	"testing"
 
 	"github.com/koding/eventexporter"
+	"github.com/koding/eventexporter/eventexportertest"
+
 	. "github.com/smartystreets/goconvey/convey"
 )
-
-type FakeExporter struct {
-	Events []*eventexporter.Event
-}
-
-func NewFakeExporter() *FakeExporter {
-	return &FakeExporter{Events: []*eventexporter.Event{}}
-}
-
-func (l *FakeExporter) Send(event *eventexporter.Event) error {
-	l.Events = append(l.Events, event)
-	return nil
-}
 
 func TestFakeExporter(t *testing.T) {
 	Convey("When using FakeExporter", t, func() {
 		event := &eventexporter.Event{Name: "test"}
 
-		fakeExporter := NewFakeExporter()
+		fakeExporter := eventexportertest.NewFakeExporter()
 		err := fakeExporter.Send(event)
 
 		Convey("Then it should save event for debugging", func() {
