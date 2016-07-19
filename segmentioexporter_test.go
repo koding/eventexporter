0a551cb542d3b80d0ee9052155936dfdba6e9e4c
mehmetalisavas <mehmetali.savas@yandex.com>
2016-07-19 20:51:48 +0200

    add MultiExported and DatadogExporter

--- segmentioexporter_test.go
+++ segmentioexporter_test.go
@@ -17,14 +17,6 @@ func TestSegmentIOExporter(t *testing.T) {
 			So(err, ShouldEqual, ErrSegmentIOUsernameEmpty)
 		})
 
-		Convey("Then it should return err if no email", func() {
-			user := &User{Username: "indianajones"}
-			event := &Event{Name: "test", User: user}
-			_, err := buildTrack(event)
-
-			So(err, ShouldEqual, ErrSegmentIOEmailEmpty)
-		})
-
 		Convey("Then it should return err if no event name", func() {
 			user := &User{Username: "indianajones", Email: "senthil@koding.com"}
 			event := &Event{Name: "", User: user}
@@ -43,10 +35,8 @@ func TestSegmentIOExporter(t *testing.T) {
 		})
 
 		Convey("Then it should successfully send event to Segment.io", func() {
-			key := os.Getenv("SEGMENTIO_KEY")
-			if key == "" {
-				key = "entersomekeyhere"
-			}
+			key := os.Getenv("SEGMENTIO_TEST_KEY")
+			So(key, ShouldNotBeBlank)
 			size := 1
 			s := NewSegmentIOExporter(key, size)
 			defer func() {
